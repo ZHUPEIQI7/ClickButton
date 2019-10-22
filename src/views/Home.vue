4ecb90e14a81b9eb09f7b51a2562fc39f5dd25bf
朱琦 <823436154@qq.com>
2019-10-22 16:32:00 +0800

    aaa

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <button id="button1" ref="target" @click="openNewPage">打开新页面</button>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import wnsdk from "@tjmt/wnsdk";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component
+
+export default class Home extends Vue {
+
+  private openNewPage() {
+    wnsdk.page.open({
+      url: "http://www.tjmeiteng.com",
+      data: {},
+      needCode: 0, 
+      success(result:any) {
+        console.log(11111111111111)
+      },
+      error(error:any) {
+        // 失败处理...
+      }
+    })
+  }
+
+}
 </script>
+
+<style scoped lang="scss">
+  
+</style>
